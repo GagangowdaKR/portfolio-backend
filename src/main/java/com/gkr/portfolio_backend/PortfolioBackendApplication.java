2b937587545da592e536c3a3a50650c0083afbc4
GagangowdaKR <gagandarshan22@gmail.com>
2026-05-23 14:22:28 +0530

    Initial Project Setup

---
+++ src/main/java/com/gkr/portfolio_backend/PortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gkr.portfolio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBackendApplication.class, args);
+	}
+
+}
